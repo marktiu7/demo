6c1a01782c61fdea4747b620f3da72f90fa570ed
marktiu71 <marktiu71@Outlook.com>
2016-09-28 15:20:15 +0800

    modey

--- dbtest/blog/models.py
+++ dbtest/blog/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class picinfo(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    date =models.DateTimeField()
+
+
+class userinfo(models.Model):
+     name = models.CharField(max_length=30)
+     email = models.EmailField()
+     memo = models.TextField()
