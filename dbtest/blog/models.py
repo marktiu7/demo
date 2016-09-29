864cab524027827c051bbd3e2fd19941b80ba784
marktiu71 <marktiu71@Outlook.com>
2016-09-29 18:04:23 +0800

    com

--- dbtest/blog/models.py
+++ dbtest/blog/models.py
@@ -2,6 +2,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Commoninfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class Student(Commoninfo):
+    home_group = models.CharField(max_length=50)
+
+    class Meta(Commoninfo.Meta):
+        db_table='Student'
+
 # Create your models here.
 class picinfo(models.Model):
     name = models.CharField(max_length=30)
