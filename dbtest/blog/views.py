340f4c517c9c20eff00a8dfeb925495e5f452cef
marktiu71 <marktiu71@Outlook.com>
2016-09-29 16:14:58 +0800

    m

--- dbtest/blog/views.py
+++ dbtest/blog/views.py
@@ -11,7 +11,7 @@ def show(request):
     user_list = models.userinfo.objects.all()
     aget =models.userinfo.objects.get(name='tom')
     userdata = models.userinfo.objects.all().values('name')
-    mo = models.userinfo.objects.all().values_list('id','memo')
+   # mo = models.userinfo.objects.all().values_list('id','memo')
     return render_to_response('index.html',locals())
 
  
