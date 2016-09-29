b53fc699442f0cc6cbd357be5800019d14645e19
marktiu71 <marktiu71@Outlook.com>
2016-09-29 17:01:11 +0800

    change

--- dbtest/blog/views.py
+++ dbtest/blog/views.py
@@ -12,6 +12,11 @@ def show(request):
     aget =models.userinfo.objects.get(name='tom')
     userdata = models.userinfo.objects.all().values('name')
    # mo = models.userinfo.objects.all().values_list('id','memo')
+
+    if request.method =='POST':
+       a_user= request.POST['a_user']
+       a_memo = request.POST['a_pwd']
+       models.userinfo.objects.creat(name=a_user,memo=a_memo)
     return render_to_response('index.html',locals())
 
  
